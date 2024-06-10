0dfe3ca4dede473ee2219b2066e95a65628dcc02
jkcachero <johnguitarizta@gmail.com>
2024-06-10 20:18:46 +0800

    Time reset

--- src/components/timer.js
+++ src/components/timer.js
@@ -21,6 +21,12 @@ export const Timer = ({ focusSubject, clearSubject }) => {
   const [isStarted, setIsStarted] = useState(false);
   const [progress, setProgress] = useState(1);
   const [minutes, setMinutes] = useState(0.1);
+  const onEnd = (reset) => {
+    Vibration.vibrate(PATTERN)
+    setIsStarted(false)
+    setProgress(1)
+    reset()
+  }
   return (
     <View style={styles.container}>
       <View style={styles.countdown}>
@@ -28,9 +34,7 @@ export const Timer = ({ focusSubject, clearSubject }) => {
           minutes={minutes}
           isPaused={!isStarted}
           onProgress={setProgress}
-          onEnd={() => {
-            Vibration.vibrate(PATTERN)
-          }}
+          onEnd={onEnd}
         />
         <View style={{ paddingTop: spacing.xxl }}>
           <Text style={styles.title}>Focusing on:</Text>
@@ -56,7 +60,7 @@ export const Timer = ({ focusSubject, clearSubject }) => {
         )}
       </View>
       <View style={styles.clearSubjectWrapper}>
-        <RoundedButton size={50} title='-' onPress={clearSubject} />
+        <RoundedButton size={80} title='Clear' onPress={clearSubject} />
       </View>
     </View>
   );
